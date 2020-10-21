67e981b48629c5ed6ffdac16c3200c61dc7adf08
Barcoaie <florinel.barcan@gmail.com>
2020-10-21 08:16:48 +0300

    added doc

--- Symbol_Table_FLCD/HashTable.h
+++ Symbol_Table_FLCD/HashTable.h
@@ -11,9 +11,23 @@ class HashTable
 private:
 	node *table[31];
 public:
+	/**
+	* constructor function of the Hash Table
+	*/
 	HashTable();
+	/**
+	* inserts an item in the hash table
+	* @param key a string, that is hashed and put in the table afterwards
+	*/
 	int insertItem(std::string key);
+	/**
+	* the hash function
+	* @param key the string that is hashed
+	*/
 	int hash_funct(std::string key);
+	/**
+	* destructor function
+	*/
 	~HashTable();
 };
 
